b28573cc0e5af4f6101bd954c39cf0e56d29d8fd
root <root@DESKTOP-H50395L.localdomain>
2021-06-16 13:42:14 +0800

    Selected the left menu into a separate object
    Selected the menu icon into a separate object
    
    Fixed bugs in the top menu

--- #src/apps/header_left/js/headerLeft.js
+++ #src/apps/header_left/js/headerLeft.js
@@ -1,17 +1,18 @@
 $(document).ready(function () {
-	$('.menu-burger').click(function (event) {
+	$('.icon-menu').click(function (event) {
 		$(this).toggleClass('active');
-		$('.menu-upper__body').toggleClass('active');
-		$('.menu-upper__link').toggleClass('active');
+		$('.header').toggleClass('active');
+		$('.menu-left__body').toggleClass('active');
+		$('.content').toggleClass('active');
 		$('.overlay').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
-	
-	$(".menu-upper__list").on("click", "a", function (event) {
+
+	$("menu-left__list").on("click", "a", function (event) {
 		event.preventDefault();
 		let id = $(this).attr('href'),
 			top = $(id).offset().top;
-		$('.menu-upper__body, .menu-upper__list, .menu-burger, .overlay').removeClass(' active');
+		$('.menu-left__body, .menu-left__list, .menu-icon, .overlay').removeClass(' active');
 		$('body').removeClass('lock');
 		$('body,html').animate({ scrollTop: top }, 1500);
 	});
