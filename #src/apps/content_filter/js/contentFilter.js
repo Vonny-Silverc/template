27b0667fecd2142b14b6d308dcfc76fd65db1d95
root <root@DESKTOP-H50395L.localdomain>
2021-06-16 10:54:18 +0800

    Passed the content filter

--- #src/apps/content_filter/js/contentFilter.js
+++ #src/apps/content_filter/js/contentFilter.js
@@ -1,15 +1,33 @@
 
-let filterSelectEl = document.getElementById('filter-id');
-let items_el = document.getElementById('items-id');
+$(document).ready(function () {
+	$(".filter-class__list").on("click", "a", function (event) {
+		event.preventDefault();
 
-filter_select_el.onchange = function() {
-	let items = items_el.getElementsByClassName('item');
+		let cat = $(this).attr('href');
 
-	for (let i=0; i<items.length; i++) {
-		if (items[i].classList.contains(this.value)) {
-			this.removeClass('_invisible');
+		$(".filter-class__button").each(function () {
+			let workCat = $(this).attr('href');
+			if (workCat != cat) {
+				$(this).removeClass('active');
+			} else {
+				$(this).addClass('active');
+			}
+		})
+
+
+		if (cat == 'all') {
+			$(".class-item").removeClass('_invisible');
+
 		} else {
-			this.addClass('_invisible');
+			$("[data-cat]").each(function () {
+				let workCat = $(this).data('cat');
+
+				if (workCat != cat) {
+					$(this).addClass('_invisible');
+				} else {
+					$(this).removeClass('_invisible');
+				}
+			});
 		}
-	}
-};
+	});
+});
